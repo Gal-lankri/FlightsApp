67bfe27f766ce45ed650340bcb05950aad150055
Gal-lankri <gal.lankri2@gmail.com>
2025-02-03 23:00:04 +0200

    Add React frontend project

--- Backend/obj/Debug/net8.0/FlightsApp.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/FlightsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc28ae89506c986d0fa75c9b8f3b48ecaf62bc33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23683f0d37c08e4af4fcf01b2628515221a26183")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
